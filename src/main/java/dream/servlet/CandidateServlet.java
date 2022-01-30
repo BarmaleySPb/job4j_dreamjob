3e043e4de508bb3d1a50ac3c1c9bd464ac281007
BarmaleySPb <e.g.zakharov@gmail.com>
2022-01-30 13:16:12 +0300

    3. Редактирование вакансии. [#277566]

--- src/main/java/dream/servlet/CandidateServlet.java
+++ src/main/java/dream/servlet/CandidateServlet.java
@@ -1,6 +1,7 @@
 package dream.servlet;
 
 import dream.model.Candidate;
+import dream.model.Post;
 import dream.store.Store;
 
 import javax.servlet.ServletException;
@@ -14,7 +15,12 @@ public class CandidateServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
-        Store.instOf().save(new Candidate(0, req.getParameter("name")));
+        Store.instOf().save(
+                new Candidate(
+                        Integer.parseInt(req.getParameter("id")),
+                        req.getParameter("name")
+                )
+        );
         resp.sendRedirect(req.getContextPath() + "/candidates.jsp");
     }
 }
