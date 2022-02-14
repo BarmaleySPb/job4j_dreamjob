b7bdadc690a25e4f9e5f364e95f9790d915854bc
BarmaleySPb <e.g.zakharov@gmail.com>
2022-02-14 18:01:50 +0300

    1. Написать тест на PostServlet. [#2518 #264146]

--- src/test/java/dream/servlet/CandidateServletTest.java
+++ src/test/java/dream/servlet/CandidateServletTest.java
@@ -5,16 +5,17 @@ import dream.store.DbStore;
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 
 public class CandidateServletTest {
+
     @Test
     public void whenCreateCandidate() throws IOException, ServletException {
         HttpServletRequest req = mock(HttpServletRequest.class);
@@ -24,5 +25,16 @@ public class CandidateServletTest {
         new CandidateServlet().doPost(req, resp);
         Candidate candidate = DbStore.instOf().findCandidateByName("name of new Candidate");
         Assert.assertNotNull(candidate);
+    }
+
+    @Test
+    public void whenDoGet() throws ServletException, IOException {
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        HttpServletResponse resp = mock(HttpServletResponse.class);
+        RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+        when(req.getRequestDispatcher("candidates.jsp")).thenReturn(dispatcher);
+        new CandidateServlet().doGet(req, resp);
+        verify(req, times(1)).getRequestDispatcher("candidates.jsp");
+        verify(dispatcher).forward(req, resp);
     }
 }
--- src/test/java/dream/servlet/PostServletTest.java
+++ src/test/java/dream/servlet/PostServletTest.java
@@ -4,18 +4,20 @@ import org.junit.Test;
 import dream.model.Post;
 import dream.store.DbStore;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import java.io.IOException;
 
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 
 public class PostServletTest {
+
     @Test
     public void whenCreatePost() throws IOException, ServletException {
         HttpServletRequest req = mock(HttpServletRequest.class);
@@ -26,5 +28,19 @@ public class PostServletTest {
         new PostServlet().doPost(req, resp);
         Post post = DbStore.instOf().findPostByName("name of new post");
         assertNotNull(post);
+    }
+
+    @Test
+    public void whenDoGet() throws ServletException, IOException {
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        HttpServletResponse resp = mock(HttpServletResponse.class);
+        HttpSession session = mock(HttpSession.class);
+        RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+        when(req.getSession()).thenReturn(session);
+        when(req.getRequestDispatcher("posts.jsp")).thenReturn(dispatcher);
+        new PostServlet().doGet(req, resp);
+        verify(req, times(1)).getRequestDispatcher("posts.jsp");
+        verify(dispatcher).forward(req, resp);
+        verify(session).getAttribute("user");
     }
 }
